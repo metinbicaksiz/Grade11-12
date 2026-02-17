3cce3204c13b29afb24f63795a3172398ba51bea
metin <metin@gobindsarvar.ca>
2026-02-17 11:17:22 -0800

    first code

---
+++ main.py
@@ -0,0 +1,24 @@
+# print('abc')
+# print("abc")
+# print("I'm from Turkey")
+# print("""Hello everyone
+# I have been here for 13 months
+# # and it always rains here""")
+# print(646 + 250)
+# print("Hello" "World" "from" "metin")
+# #
+# print("Hello" + " " + "World" + " " + "from" + " " + "metin")
+# print("Hello" " World" " from" " metin")
+# print("Hello " * 6)
+
+age = int(input("How old are you?\n"))
+print(age)
+
+# Make sure your variable names are descriptive
+# Don't have spaces between words
+# Don't start with numbers
+# Don't use special words like print or input
+# Choose simple words that are less likely to become typos
+# Check the company style guidelines if you start work at a company
+
+firstName = input("What is your first name?\n")
