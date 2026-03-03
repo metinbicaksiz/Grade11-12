cbc0e6ad4ccb141e7cd8814c608643e15a9bda9f
certi2x <a.toor@gobindsarvar.ca>
2026-03-03 10:49:22 -0800

    python pizza

--- .idea/TIP CALCULATOR.py
+++ .idea/TIP CALCULATOR.py
@@ -1,12 +1,13 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-tipPercemt = tip/ 100
-tipamount = bill * tipPercent
-totalbill = bill + tipamount
-per_person = totalbill / people
+grade = int(input("enter your grade"))
 
-print(f"Each person should pay: ${per_person:.2f}")
-print(f"Each person should pay: ${int(per_person):.2f}")
+if grade < 50:
+    print("your grade is emerging")
+elif grade < 65:
+    print("your grade is developing")
+elif grade < 85:
+    print("your grade is proficient")
+elif grade < 100:
+    print("your grade is extending")
+
+
 
