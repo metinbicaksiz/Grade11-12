d50bc44342e64813a1ecb05a69635bc017168638
certi2x <a.toor@gobindsarvar.ca>
2026-03-09 11:06:59 -0700

    rock paper scissors

---
+++ .idea/jst.py
@@ -0,0 +1,58 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+images = [rock, paper, scissors]
+computerChoice = random.randint(0, len(images) - 1)
+
+userchoice = int(input("what do you choose? Type 0 for rock, 1 for paper or 2 for scissors. \n"))
+print(images[userchoice])
+print("computer chose: ")
+print(images[computerChoice])
+if userchoice == 0:
+    if computerChoice == 0:
+        print("It is a draw!")
+    elif computerChoice == 1:
+        print("You loose!")
+    elif computerChoice == 2:
+        print("You win!")
+
+if userchoice == 1:
+    if computerChoice == 0:
+        print("You win!")
+    elif computerChoice == 1:
+        print("Its a draw")
+    elif computerChoice == 2:
+        print("You loose!")
+
+    if userChoice == 2:
+        if computerChoice == 0:
+            print ("You loose!")
+        elif compuetChoice == 1:
+            print("You win!")
+        elif computerChoice == 2:
+            print("It is a draw")
+
